b43e1bcf428649996bdef7f511f82197daac3017
huangjian888 <465265897@qq.com>
2018-08-24 11:38:20 +0800

    补全1.0版本

---
+++ src/main/java/cn/jeeweb/modules/sys/service/impl/DataSourceServiceImpl.java
@@ -0,0 +1,22 @@
+package cn.jeeweb.modules.sys.service.impl;
+
+import cn.jeeweb.core.common.service.impl.CommonServiceImpl;
+import cn.jeeweb.modules.sys.entity.DataSource;
+import cn.jeeweb.modules.sys.mapper.DataSourceMapper;
+import cn.jeeweb.modules.sys.service.IDataSourceService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**   
+ * @Title: 数据源
+ * @Description: 数据源
+ * @author jeeweb
+ * @date 2017-05-10 12:01:57
+ * @version V1.0   
+ *
+ */
+@Transactional
+@Service("dataSourceService")
+public class DataSourceServiceImpl extends CommonServiceImpl<DataSourceMapper,DataSource> implements IDataSourceService {
+
+}
